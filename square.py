9abdd6d10dd59c8266fcbf44808ae59bb6e234c6
shafrinvino <shafrin@msec.edu.in>
2026-06-17 12:00:26 +0530

    Update square.py

--- square.py
+++ square.py
@@ -1,14 +1,2 @@
- # square.py
-
 def square(number):
     return number * number
-
-
-def main():
-    number = int(input("Enter a number: "))
-    result = square(number)
-    print("The square of", number, "is", result)
-
-
-if __name__ == "__main__":
-    main()
