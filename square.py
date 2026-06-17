58889216b0611528c2dda80b16c0d55ace0e1f32
shafrinvino <shafrin@msec.edu.in>
2026-06-17 10:50:36 +0530

    Add files via upload

---
+++ square.py
@@ -0,0 +1,7 @@
+# Square Number Calculator
+
+number = int(input("Enter a number: "))
+
+square = number * number
+
+print("The square of", number, "is", square)
---
+++ test.py
@@ -0,0 +1,6 @@
+def square(number):
+    return number * number
+
+if __name__ == "__main__":
+    num = int(input("Enter a number: "))
+    print("The square of", num, "is", square(num))
